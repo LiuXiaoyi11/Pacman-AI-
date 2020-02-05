80a20666c0b1fe064ba743477faef7ac4fbe3772
Xiaoyi Liu <46695105+LiuXiaoyi11@users.noreply.github.com>
2020-02-04 21:58:48 -0500

    Add files via upload

---
+++ Search/search.py
@@ -0,0 +1,255 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+import util
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+'''
+class SearchNode:
+    def __init__(self, stateTuple, previousNode):
+        self.stateData = stateTuple
+        self.prev = previousNode
+    
+    def fullActionInfo(self):
+        return self.stateData
+    
+    def stateInfo(self):
+        return self.stateData[0]
+    
+    def directionToReach(self):
+        return self.stateData[1]
+    
+    def cost(self):
+        return self.stateData[2]
+
+    def previousNode(self):
+        return self.prev
+
+def graphSearch(problem, queueingStructure):   
+    covered = set([])
+    
+    firstTuple = (problem.getStartState(), 'Stop', 0)
+    startNode = SearchNode(firstTuple, None)
+    
+    queueingStructure.push(startNode)
+    covered.add(startNode.stateInfo())
+    
+    while not queueingStructure.isEmpty():
+        n = queueingStructure.pop()
+       
+        if problem.isGoalState(n.stateInfo()):
+            searchResult = []
+            while True:
+                if n.previousNode() is None: break
+                searchResult.append(n.directionToReach())
+                n = n.previousNode()
+            searchResult.reverse()
+            return searchResult
+        
+        for sTuple in problem.getSuccessors(n.stateInfo()):
+            if not (sTuple[0] in covered):
+                queueingStructure.push(SearchNode(sTuple, n))
+                covered.add(sTuple[0])
+        
+    return []
+    '''
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print("Start:", problem.getStartState())
+    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+    """
+    "*** YOUR CODE HERE ***"
+    fringe = util.Stack()
+    covered = set()
+    startLocation = problem.getStartState()
+    startNode = (startLocation, [])
+    fringe.push(startNode)
+
+    while not fringe.isEmpty():
+        node = fringe.pop()
+        covered.add(node[0])
+        if problem.isGoalState(node[0]):
+            return node[1]
+        successors = problem.getSuccessors(node[0])
+        for item in successors:
+            if item[0] in covered:
+                continue
+            fringe.push((item[0], node[1] + [item[1]]))
+    return None
+
+    ''' 
+        fringe = util.fringe()
+        results = graphSearch(problem, fringe)
+        return results
+    "util.raiseNotDefined()"
+    '''
+
+def breadthFirstSearch(problem):
+    """Search the shallowest nodes in the search tree first."""
+    "*** YOUR CODE HERE ***"
+    fringe = util.Queue()
+    covered = set()
+    startLocation = problem.getStartState()
+    covered.add(startLocation)
+    startNode = (startLocation, [])
+    fringe.push(startNode)
+
+    while not fringe.isEmpty():
+        node = fringe.pop()
+        if problem.isGoalState(node[0]):
+            return node[1]
+        successors = problem.getSuccessors(node[0])
+        for item in successors:
+            if item[0] in covered:
+                continue
+            covered.add(item[0])
+            fringe.push((item[0], node[1] + [item[1]]))
+    return None
+    '''
+    queue = util.Queue()
+    results = graphSearch(problem, queue)
+    return results
+    "util.raiseNotDefined()"
+    '''
+
+def uniformCostSearch(problem):
+    """Search the node of least total cost first."""
+    "*** YOUR CODE HERE ***"
+    fringe = util.PriorityQueue()
+    covered = set()
+    startLocation = problem.getStartState()
+    startNode = (startLocation, [], 0)
+    fringe.push(startNode, 0)
+
+    while not fringe.isEmpty():
+        node = fringe.pop()
+        if problem.isGoalState(node[0]):
+            return node[1]
+        if node[0] not in covered:
+            covered.add(node[0])
+            for successor in problem.getSuccessors(node[0]):
+                if successor[0] not in covered:
+                    cost = node[2] + successor[2]
+                    fringe.push((successor[0], node[1] + [successor[1]], cost), cost)
+    return None
+    '''
+    priorityQueue = util.PriorityQueueWithFunction(getNodeCost)
+    results = graphSearch(problem, priorityQueue)
+    return results
+    "util.raiseNotDefined()"
+    '''
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "*** YOUR CODE HERE ***"
+    fringe = util.PriorityQueue()
+    covered = set()
+    startLocation = problem.getStartState()
+    startNode = (startLocation, [], 0)
+    fringe.push(startNode, 0)
+
+    while not fringe.isEmpty():
+        node = fringe.pop()
+        if problem.isGoalState(node[0]):
+            return node[1]
+        if node[0] not in covered:
+            covered.add(node[0])
+            for successor in problem.getSuccessors(node[0]):
+                if successor[0] not in covered:
+                    cost = node[2] + successor[2]
+                    totalcost = cost + heuristic(successor[0], problem)
+                    fringe.push((successor[0], node[1] + [successor[1]], cost), totalcost)
+    return None
+    '''
+    priorityQueue = util.PriorityQueueWithFunction(lambda node: aStarPriorityFunction(problem, node, heuristic))
+    results = graphSearch(problem, priorityQueue)
+    return results
+    "util.raiseNotDefined()"
+    '''
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
